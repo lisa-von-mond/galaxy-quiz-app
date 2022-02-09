693d426b8eb9a84f8732f786f37082fd26cc3cd9
Lisa Lu <lisalue@posteo.de>
2022-02-09 16:15:16 +0100

    build basic structure

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import `../styles.css`;
+import "../style.css";
 
 
 export const parameters = {
--- styles/components/card.stories.js
+++ styles/components/card.stories.js
@@ -1,9 +1,31 @@
 export default {
-    title "card",
+    title: "components/card",
 };
 
 
-export const Default = () =>`
-<h1>Galaxy Quiz App</h1>`
-;
+export const Default = () =>
+`<div class="card first">
+<div class="question-text">
+    <h2>What is your favourite planet?</h2>
+    <p>For over 1000 years, Jedis and space bees have fightened for freedom in all galaxies in our universe. Name your favourite planet of this beautiful cosmos!</p>
+</div>
+<div class="show-answer-button">
+    <button>show answer</button>
+</div>
+<div class="tags">
+    <div class="tag">ONE</div>
+    <div class="tag">TWO</div>
+    <div class="tag">THREE</div>
+    <div class="tag">FOUR</div>
+    <div class="tag">FIVE</div>
+    <div class="tag">SIX</div>
+    <div class="tag">SEVEN</div>
+</div>
+</div>
+`;
+
+import DefaultHtml from "./card1.html";
+
+export const Card1 = () => DefaultHtml;
+
 
