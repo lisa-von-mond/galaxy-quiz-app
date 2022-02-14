25897be5d4b1dc394b6cef9b1fe9707e43be8049
Lisa Lu <lisalue@posteo.de>
2022-02-14 20:20:46 +0100

    make first js

---
+++ script.js
@@ -0,0 +1,24 @@
+let answerTextOne = document.querySelector("#answer-text-1");
+let answerButtonOne = document.querySelector("#answer-button-1");
+
+answerButtonOne.addEventListener("click", () => {
+    answerTextOne.classList.remove("answer-text-hidden");
+});
+
+
+
+
+let answerTextTwo = document.querySelector("#answer-text-2");
+let answerButtonTwo = document.querySelector("#answer-button-2");
+
+answerButtonTwo.addEventListener("click", () => {
+    answerTextTwo.classList.remove("answer-text-hidden");
+});
+
+
+let answerTextThree = document.querySelector("#answer-text-3");
+let answerButtonThree = document.querySelector("#answer-button-3");
+
+answerButtonThree.addEventListener("click", () => {
+    answerTextThree.classList.remove("answer-text-hidden");
+});
