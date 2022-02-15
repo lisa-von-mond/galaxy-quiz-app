d22236824eec1d1581d7e4795cd864353d94f047
Lisa Lu <lisalue@posteo.de>
2022-02-15 11:01:24 +0100

    improve js functionality

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-let answerTextOne = document.querySelector("#answer-text-1");
-let answerButtonOne = document.querySelector("#answer-button-1");
+let answerTextOne = document.querySelector('[data-js="answer-text-1"]');
+let answerButtonOne = document.querySelector('[data-js="answer-button-1"]');
 
 answerButtonOne.addEventListener("click", () => {
-    answerTextOne.classList.remove("answer-text-hidden");
+    answerTextOne.classList.toggle("answer-text-hidden");
 });
 
-
-
-
-let answerTextTwo = document.querySelector("#answer-text-2");
-let answerButtonTwo = document.querySelector("#answer-button-2");
+let answerTextTwo = document.querySelector('[data-js="answer-text-2"]');
+let answerButtonTwo = document.querySelector('[data-js="answer-button-2"]');
 
 answerButtonTwo.addEventListener("click", () => {
-    answerTextTwo.classList.remove("answer-text-hidden");
+    answerTextTwo.classList.toggle("answer-text-hidden");
 });
 
-
-let answerTextThree = document.querySelector("#answer-text-3");
-let answerButtonThree = document.querySelector("#answer-button-3");
+let answerTextThree = document.querySelector('[data-js="answer-text-3"]');
+let answerButtonThree = document.querySelector('[data-js="answer-button-3"]');
 
 answerButtonThree.addEventListener("click", () => {
-    answerTextThree.classList.remove("answer-text-hidden");
+    answerTextThree.classList.toggle("answer-text-hidden");
 });
