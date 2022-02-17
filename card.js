295c9d466a6bc4d45e09771bbb1c4e570a7e666a
Lisa Lu <lisalue@posteo.de>
2022-02-17 15:05:36 +0100

    keep calm and commit

--- card.js
+++ card.js
@@ -1,20 +1,39 @@
 let answerTextOne = document.querySelector('[data-js="answer-text-1"]');
 let answerButtonOne = document.querySelector('[data-js="answer-button-1"]');
 
-answerButtonOne.addEventListener("click", () => {
+answerButtonOne?.addEventListener("click", () => {
     answerTextOne.classList.toggle("answer-text-hidden");
 });
 
 let answerTextTwo = document.querySelector('[data-js="answer-text-2"]');
 let answerButtonTwo = document.querySelector('[data-js="answer-button-2"]');
 
-answerButtonTwo.addEventListener("click", () => {
+answerButtonTwo?.addEventListener("click", () => {
     answerTextTwo.classList.toggle("answer-text-hidden");
 });
 
 let answerTextThree = document.querySelector('[data-js="answer-text-3"]');
 let answerButtonThree = document.querySelector('[data-js="answer-button-3"]');
 
-answerButtonThree.addEventListener("click", () => {
+answerButtonThree?.addEventListener("click", () => {
     answerTextThree.classList.toggle("answer-text-hidden");
+});
+
+
+const bookMark1 = document.querySelector('[js-data="bookmark-first"]');
+
+bookMark1?.addEventListener("click", () => {
+    bookMark1.classList.toggle("bookmarked");
+});
+
+const bookMark2 = document.querySelector('[js-data="bookmark-second"]');
+
+bookMark2?.addEventListener("click", () => {
+    bookMark2.classList.toggle("bookmarked");
+});
+
+const bookMark3 = document.querySelector('[js-data="bookmark-third"]');
+
+bookMark3?.addEventListener("click", () => {
+    bookMark3.classList.toggle("bookmarked");
 });
