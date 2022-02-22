916dfd6af0005291d16face191f3ab1ea5075f6b
Lisa Lu <lisalue@posteo.de>
2022-02-22 09:15:47 +0100

    make one page html

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,7 +1,31 @@
-const menuHome = x.querySelector('data-js="menu-home"]');
+export const menuHome = document.querySelector('[data-js="menu-home"]');
+export const menuForm = document.querySelector('[data-js="menu-form"]');
+export const allCards = document.querySelector('[js-data="cards"]');
+export const wholeForm = document.querySelector('[js-data="form"]');
+
 
 menuHome.addEventListener('click', () => {
-  menuHome.classList.toggle('bookmarked');
 
-}};
+  menuHome.classList.add('now');
+  menuForm.classList.remove('now');
+  allCards.classList.add('menu-active')
+  allCards.classList.remove('menu-inactive')
+  wholeForm.classList.add('menu-inactive')
+  wholeForm.classList.remove('menu-active')
+
+});
+
+menuForm.addEventListener('click', () => {
+
+  menuHome.classList.toggle('now');
+  menuForm.classList.toggle('now');
+  allCards.classList.add('menu-inactive')
+  allCards.classList.remove('menu-active')
+  wholeForm.classList.add('menu-active')
+  wholeForm.classList.remove('menu-inactive')
+
+
+});
+
+
 
--- script.js
+++ script.js
@@ -6,4 +6,10 @@ import {
 } from './js/components/form.js';
 import { answerBlock } from './js/components/card.js';
 import { bookMarkArea } from './js/components/bookmark.js';
+import { menuHome, menuForm } from './js/components/menu.js';
+
+
+
+
+
 
